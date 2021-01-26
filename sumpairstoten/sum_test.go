3f47fa4d4c66ad61b99567631ac72e9a8cb3b55e
Mike Camp <Mike.Camp@sas.com>
2021-01-26 18:55:50 -0500

    test: add test cases to sumpairstoten

--- sumpairstoten/sum_test.go
+++ sumpairstoten/sum_test.go
@@ -9,4 +9,6 @@ import (
 func TestPairSumIsTen(t *testing.T) {
 	assert.Equal(t, []int{1, 9}, PairSumIsTen([]int{1, 2, 5, 6, 9, 5}))
 	assert.Equal(t, []int{-2, 12}, PairSumIsTen([]int{1, -2, 5, 12, 6, 8}))
+	assert.Nil(t, PairSumIsTen(nil))
+	assert.Nil(t, PairSumIsTen([]int{1, 4, -5, 3, 4}))
 }
